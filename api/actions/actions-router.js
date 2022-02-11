69df4c2ff183bf75d5010b871494d2d6f4e2e519
david-aihe <doa9200@gmail.com>
2022-02-11 12:04:43 -0500

    Fixed errors

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,25 +1,36 @@
 const express = require('express');
+const ActionsModel = require('./actions-model')
 
 const router = express.Router();
 
 router.get('/', (req, res, next) =>{
-
+    ActionsModel.get()
+        .then(actions =>{
+            console.log(actions);
+        })
+        .catch(next)
 })
 
 router.get('/:id', (req, res, next) =>{
-    
+    next()
 })
 
 router.post('/', (req, res, next) =>{
-    
+    next()
 })
 
 router.put('/:id', (req, res, next) =>{
-    
+    next()
 })
 
 router.delete('/:id', (req, res, next) =>{
-    
+    next()
 })
+
+router.use((err, req, res, next) =>{
+    res.status(err.status || 500).json({
+      message: err.message,
+    stack: err.stack})
+  })
 
 module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,30 +1,57 @@
 const express = require('express');
 const {validateProject, } = require('./projects-middleware')
+const ProjectsModel = require('./projects-model')
 
 const router = express.Router();
 
 router.get('/', (req, res, next) =>{
-
+    ProjectsModel.get()
+        .then(projects => {
+            console.log(projects);
+        })
+        .catch((err) => {
+            console.log('it didnt work');
+            next()
+        });
 })
 
 router.get('/:id', (req, res, next) =>{
-    
+    next()
 })
 
-router.post('/', validateProject, (req, res, next) =>{
-    
+router.post('/', (req, res, next) =>{
+    const {name, description} = req.body;
+    if(!name || !description){
+        res.status(400).json({message: "Please provide name and description for the project"})
+    }
+    else {
+        ProjectsModel.insert(req.body)
+            .then(result => {
+                console.log(result);
+                //res.status(201).json(result))
+            })
+            .catch((err) => {
+                console.log('wasnt able to post');
+            });
+    }
 })
 
 router.put('/:id', validateProject, (req, res, next) =>{
-    
+    next()
 })
 
 router.delete('/:id', (req, res, next) =>{
-    
+    next()
 })
 
 router.get('/:id/actions', (req, res, next) =>{
-    
+    next()
 })
+
+router.use((err, req, res, next) =>{
+    res.status(err.status || 500).json({
+      message: err.message,
+    stack: err.stack})
+  })
 
 module.exports = router;
