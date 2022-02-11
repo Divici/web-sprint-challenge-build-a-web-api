81c661ab24534f424102807587027ea43f461c76
david-aihe <doa9200@gmail.com>
2022-02-11 10:17:05 -0500

    projects validateProject middleware added

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,14 @@
-// add middlewares here related to projects
+function validateProject(req, res, next) {
+    const {name} = req.body;
+    if(!name || !name.trim()){
+        res.status(400).json({message: "Required name field is missing"})
+    }
+    else{
+        req.name = name.trim()
+        next();
+    }
+}
+
+module.exports = {
+    validateProject,
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,30 @@
-// Write your "projects" router here!
+const express = require('express');
+const {validateProject, } = require('./projects-middleware')
+
+const router = express.Router();
+
+router.get('/', (req, res, next) =>{
+
+})
+
+router.get('/:id', (req, res, next) =>{
+    
+})
+
+router.post('/', validateProject, (req, res, next) =>{
+    
+})
+
+router.put('/:id', validateProject, (req, res, next) =>{
+    
+})
+
+router.delete('/:id', (req, res, next) =>{
+    
+})
+
+router.get('/:id/actions', (req, res, next) =>{
+    
+})
+
+module.exports = router;
